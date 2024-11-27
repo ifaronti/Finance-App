f6fc5b09eaa1dd5f2cb6a26167e75a8e3592dd03
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-11-27 12:30:15 +0000

    suspense boundary returned

--- finance-app/app/oauth/layout.tsx
+++ finance-app/app/oauth/layout.tsx
@@ -1,4 +1,6 @@
-
+import { Suspense } from "react"
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return <div>{children}</div>
+    return <Suspense fallback={<div>Loading...</div>}>
+        <div>{children}</div>
+    </Suspense>
 }
