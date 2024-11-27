cf4a1700cb8bd15a58d99a1853372275991410ee
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-11-27 13:55:15 +0000

    suspense boundary expanded

--- finance-app/app/dashboard/layout.tsx
+++ finance-app/app/dashboard/layout.tsx
@@ -1,21 +1,24 @@
 import NavSideBar from "@/components/nav";
 import ShowbarProvider from "@/providers/showBarContext";
 import MainHeader from "@/components/accountHeader";
+import { Suspense } from "react";
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <ShowbarProvider>
-      <section className="flex flex-col-reverse gap-6 sm:gap-8 xl:gap-10 2xl:flex-row">
-        <nav>
-          <NavSideBar/>
-        </nav>
-        <div className="h-screen pt-8 overflow-y-scroll no-scrollbar">
-          <MainHeader/>
-          {children}
-        </div>
-      </section>
-    </ShowbarProvider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ShowbarProvider>
+        <section className="flex flex-col-reverse gap-6 sm:gap-8 xl:gap-10 2xl:flex-row">
+          <nav>
+            <NavSideBar/>
+          </nav>
+          <div className="h-screen pt-8 overflow-y-scroll no-scrollbar">
+            <MainHeader/>
+            {children}
+          </div>
+        </section>
+        </ShowbarProvider>
+      </Suspense>
   );
 }
 
