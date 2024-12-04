b90c83c0acaf3ccc5de3812a917c5be6037dcf9a
ifaronti <135629496+ifaronti@users.noreply.github.com>
2024-12-04 15:57:44 +0000

    added homepage texts

--- finance-app/app/page.tsx
+++ finance-app/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { largeLogo } from "@/components/svgAssets";
 
 export default function Home() {
 
@@ -7,14 +8,26 @@ export default function Home() {
   
   return (
     <section className="w-full flex items-center justify-between 2xl:justify-start gap-[140px]">
-      <div className="hidden p-5 w-[600px] 2xl:block">
+      <div className="hidden p-5 w-[600px] relative 2xl:block">
         <Image
           width={560}
           height={920}
           src={"/assets/images/illustration-authentication.svg"}
           alt="Home Image"
-          className=""
+          className=" rounded-lg"
         />
+        <div className="absolute h-full top-0 py-20 px-10 z-80">
+          {largeLogo}
+          <div className="absolute bottom-20">
+            <h1 className="text-[32px] mb-4 w-[400px] font-bold text-white">
+              Keep track of your money and save for your future
+            </h1>
+            <p className="text-[14px] text-white">
+              Personal finance app puts you in control of your spending.
+              Track transactions, set budgets, and add to savings pots easily.
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className="text-white 2xl:w-[unset] 2xl:h-[unset] w-full h-[100vh] items-center justify-center flex flex-col gap-3">
